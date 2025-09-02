38399f4437cda71044d31dd76e24a1bf31d71b67
junyouu <junyou041129@gmail.com>
2025-09-02 17:32:01 +0800

    [Add] Community Forum Feature
    Community Forum feature with comment and share story functions
    
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:here4u/core/widgets/bottom_navibar.widget.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
