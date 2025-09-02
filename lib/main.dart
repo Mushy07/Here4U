03912163c152e849bf2e5ae28f3c352ebebf23c8
junyouu <junyou041129@gmail.com>
2025-09-02 17:32:01 +0800

    [Add] Community Forum Feature
    Community Forum feature with comment and share story functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:here4u/features/registration/presentation/screens/registration_landing.screen.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
