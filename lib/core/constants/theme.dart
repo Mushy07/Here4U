63d27e54aecec5bf141111020dcb4a6a8a457f2c
yikweii <yikweii1234@gmail.com>
2025-09-03 14:41:23 +0800

    Main page and diary page

---
+++ lib/core/constants/theme.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const primaryClr = Color(0xFFD6EEDF);
+TextStyle get subHeadingStyle {
+  return GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+TextStyle get heading2Style {
+  return GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+TextStyle get heading4Style {
+  return GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+TextStyle get whiteHeading3Style {
+  return GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 14,
+      color: Colors.white,
+
+    ),
+  );
+}
+
+TextStyle get heading3Style {
+  return GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+
+TextStyle get diaryEmojiStyle {
+  return GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+
+TextStyle get diaryEmotionStyle {
+  return GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      color: Colors.blue,
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    ),
+  );
+}
+
