cd984e6c7423766df798a1e1b8a19be065b84f57
Mushy07 <chaijenson55@gmail.com>
2025-09-06 21:32:49 +0800

    [FIX] main reroute

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegistrationLandingScreen(),
+      home: BottomNaviBar(),
       theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
     );
   }
