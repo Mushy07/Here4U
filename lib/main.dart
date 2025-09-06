aa0a1ee0226807a34144c8b9d896026f68742550
junyouu <junyou041129@gmail.com>
2025-09-06 17:51:15 +0800

    [DELETE] some useless line in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:here4u/core/widgets/bottom_navibar.widget.dart';
-import 'features/user/community_forum/presentation/screens/community_forum.screen.dart';
-import 'features/user/diary/presentation/screens/mood_diary.screen.dart';
 
 void main() {
   runApp(const MyApp());
